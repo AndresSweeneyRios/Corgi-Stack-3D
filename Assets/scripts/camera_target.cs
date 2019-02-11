e80f2efe5426aa1a34b6a51b91443a5a648bc2ab
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-02-11 15:55:33 -0800

    Project Push

---
+++ Assets/scripts/camera_target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_target : MonoBehaviour
+{
+    private RaycastHit hit;
+    private int layerMask = ~(1 << 2);
+    public float distance;
+
+    void Update() {
+        Vector3 position = transform.position;
+        Vector3 direction = transform.TransformDirection(Vector3.right);
+        
+        if (Physics.Raycast(position, direction, out hit, distance, layerMask)) {
+            Debug.DrawRay(position, direction * hit.distance, Color.yellow);
+            this.gameObject.transform.GetChild(0).gameObject.transform.position = hit.point;
+        } else {
+            Debug.DrawRay(position, direction * distance, Color.white);
+            this.gameObject.transform.GetChild(0).gameObject.transform.position = transform.position + (direction * distance);
+        }
+    }
+}
